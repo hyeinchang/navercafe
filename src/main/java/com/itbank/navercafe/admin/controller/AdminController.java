246be7c20f23d2c92d5b010d24a555dca3f7699e
Hyein Chang <chi9148@gmail.com>
2022-09-21 04:00:04 +0900

    jsonArray ajax test

--- src/main/java/com/itbank/navercafe/admin/controller/AdminController.java
+++ src/main/java/com/itbank/navercafe/admin/controller/AdminController.java
@@ -1,10 +1,21 @@
 package com.itbank.navercafe.admin.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+import com.itbank.navercafe.admin.deco.dto.TestDTO;
 import com.itbank.navercafe.user.cafe.dto.CafeDTO;
 
 @Controller
@@ -15,5 +26,43 @@ public class AdminController {
 		System.out.println("cafeId : " + cafeDTO.getCafeId());
 		model.addAttribute("cafeDTO", cafeDTO);
 		return "admin/index";
+	}
+	
+	@RequestMapping(value="/test/jsonArrayAjax")
+	public String testJasonArrayAjax() {
+		return "admin/test/jsonArrayAjax";
+	}
+	
+	@RequestMapping(value="/test/jsonArrayAjax/act", produces="application/json")
+	@ResponseBody
+	public List<TestDTO> testJasonArrayAjaxAct(@RequestBody String data) {
+		List<TestDTO> result = new ArrayList<TestDTO>();
+		
+		try {
+			JSONParser jsonParser = new JSONParser();
+			
+			JSONArray jsonArray = (JSONArray) jsonParser.parse(data);
+			
+			Iterator it = jsonArray.iterator();
+			
+			int count = 1;
+			
+			while(it.hasNext()) {
+				Gson gson = new Gson();
+				JSONObject jsonObject = (JSONObject) it.next();
+				TestDTO testDTO = gson.fromJson(jsonObject.toString(), TestDTO.class);
+				
+				System.out.println("testDTO" + count + " -- hello : " + testDTO.getHello());
+				System.out.println("testDTO" + count + " -- bye : " + testDTO.getBye());
+				
+				result.add(testDTO);
+				
+				count++;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
--- src/main/webapp/WEB-INF/views/admin/test/jsonArrayAjax.jsp
+++ src/main/webapp/WEB-INF/views/admin/test/jsonArrayAjax.jsp
@@ -1,15 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
-</html><%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <c:set var="contextPath" value="${pageContext.request.contextPath}"/>
 
@@ -51,22 +40,36 @@
       		</ul>
       		
 <script type="text/javascript">
+var testList = new Array();
+var testFormList = document.testForm;
 
+for(var i=0; i<testFormList.length; i++) {
+	var testForm = testFormList[i];
+	var obj = new Object();
+	
+	for(var j=0; j<testForm.length; j++) {
+		var element = testForm[j];
+		
+		if(element.name) {
+			obj[element.name] = element.value;
+		}
+	}
+	testList.push(obj);
+}
 
 var data = JSON.stringify(testList);
-var params = {};
 
 console.log("testList : " + testList)
 console.log("data : " + data);
+
 $.ajax({
 	type : 'post'
 	, contentType : 'application/json'
-	, url : '${contextPath}/admin/deco/test'
-	, traditional : true
+	, url : '${contextPath}/admin/test/jsonArrayAjax/act'
+	//, traditional : true
 	, dataType : 'json'
-	//, data : {data : JSON.stringify(params)}
 	, data : data
-	, success : function(testList) {
+	, success : function(data) {
 		console.log(data);
 	}
 });
