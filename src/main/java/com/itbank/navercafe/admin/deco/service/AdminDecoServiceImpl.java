97710396a692d8927ee9e898e13adcb2d9ccb372
Hyein Chang <chi9148@gmail.com>
2022-09-28 00:58:32 +0900

    mapper 생성 및 service 파일명 수정

--- src/main/java/com/itbank/navercafe/admin/deco/service/AdminDecoServiceImpl.java
+++ src/main/java/com/itbank/navercafe/admin/deco/service/AdminDecoServiceImpl.java
@@ -1,12 +1,16 @@
 package com.itbank.navercafe.admin.deco.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.itbank.navercafe.admin.deco.mapper.AdminDecoMapper;
 import com.itbank.navercafe.user.cafe.dto.CafeDTO;
 
 @Service
 public class AdminDecoServiceImpl implements AdminDecoService {
-
+	@Autowired
+	AdminDecoMapper decoMapper;
+	
 	@Override
 	public int saveTitle(CafeDTO cafeDTO) throws Exception {
 		// TODO Auto-generated method stub
---
+++ src/main/java/com/itbank/navercafe/user/board/service/BoardService.java
@@ -0,0 +1,5 @@
+package com.itbank.navercafe.user.board.service;
+
+public interface BoardService {
+
+}
