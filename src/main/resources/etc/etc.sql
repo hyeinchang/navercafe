75e300a4f89ac185157a651c07d3817d489e6b40
hyein <wogml603@naver.com>
2022-09-29 16:46:27 +0900

    파일 추가

--- src/main/resources/etc/etc.sql
+++ src/main/resources/etc/etc.sql
@@ -1,8 +1,19 @@
 -- project 관련 테이블 drop sql 조회
-SELECT 'drop table ' || table_name || ';' drop_project_table FROM all_tables WHERE LOWER(table_name) LIKE 'project_%';
+SELECT 'drop table ' || table_name || ';' drop_project_table FROM user_tables 
+WHERE LOWER(table_name) LIKE 'project_%';
+
+-- project 관련 테이블 시퀀스 drop sql 조회
+SELECT 'drop sequence ' || table_name || '_seq;' drop_project_table FROM user_tables 
+WHERE LOWER(table_name) LIKE 'project_%';
 
 -- 일련번호가 있는 테이블 create sequence문 조회
-SELECT 'create sequence ' || LOWER(table_name) || '_seq;' create_table_seq FROM all_tables WHERE REGEXP_LIKE(LOWER(table_name), '^project_') AND NOT REGEXP_LIKE(LOWER(table_name), 'likes|members|member_grade|cafe_info|upgrade_board|black_list|join_question');
+SELECT 'create sequence ' || LOWER(table_name) || '_seq;' create_table_seq FROM user_tables 
+WHERE REGEXP_LIKE(LOWER(table_name), '^project_') AND NOT REGEXP_LIKE(LOWER(table_name), 'likes|members|member_grade|cafe_info|upgrade_board|black_list|join_question');
 
 -- project_file 테이블의 file_num을 제외한 create sequence문 조회
-SELECT 'create sequence ' || LOWER(column_name) || '_seq;' create_file_col_seq FROM  user_tab_cols where LOWER(table_name) LIKE 'project_file' AND REGEXP_LIKE(LOWER(column_name), 'num') AND LOWER(column_name) != 'file_num';
+SELECT 'create sequence ' || LOWER(column_name) || '_seq;' create_file_col_seq FROM  user_tab_cols 
+WHERE LOWER(table_name) LIKE 'project_file' AND REGEXP_LIKE(LOWER(column_name), 'num') AND NOT LOWER(column_name) IN ('file_num', 'board_num', 'reply_num', 'memo_reply_num');
+
+-- project 관련 테이블 시퀀스 drop sql 조회
+SELECT 'drop sequence ' || sequence_name || ';' drop_project_sequence FROM user_sequences 
+WHERE LOWER(sequence_name) LIKE 'project_%' OR LOWER(sequence_name) IN ('cafe_title_num_seq', 'cafe_user_image_num_seq', 'cafe_icon_num_seq');
